cca75e85184f07d36c3afc574032c22148d54f26
Madhav Milan <49267441+m2-madhav@users.noreply.github.com>
2020-11-13 15:52:34 +0530

    code-added

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import "./../styles/App.css";
+import ToDoList from "./ToDoLists";
 
-function App() 
-{
-	return (
-	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
-	</div>
-	);
+function App() {
+  const [inputList, setInputList] = useState(" ");
+
+  const itemEvent = (event) => {
+    setInputList(event.target.value);
+  };
+
+  const [Items, setItems] = useState([]);
+  const listOfItems = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList];
+    });
+    setInputList(" ");
+  };
+
+  const deleteItem = (id) => {
+    console.log("deleted");
+
+    setItems((oldItems) => {
+      return oldItems.filter((arrEle, index) => {
+        return index !== id;
+      });
+    });
+  };
+
+  return (
+    <>
+      <div id="main">
+        <div id="center_div">
+          <br />
+          <h1>ToDO List</h1>
+          <br />
+          <input
+            type="text"
+            placeholder="Add an items"
+            value={inputList}
+            onChange={itemEvent}
+          />
+          <button onClick={listOfItems}> + </button>
+
+          <ol>
+            {Items.map((itemVal, index) => {
+              return (
+                <ToDoList
+                  key={index}
+                  id={index}
+                  text={itemVal}
+                  onSelect={deleteItem}
+                />
+              );
+            })}
+          </ol>
+        </div>
+      </div>
+    </>
+  );
 }
-
 
 export default App;
